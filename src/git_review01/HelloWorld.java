b04c0f2a4c9b623cc2887c345c3ce64fe59a9974
seymaSu <seymasonmez14@gmail.com>
2024-01-25 19:11:02 -0500

    Hello World

--- src/git_review01/HelloWorld.java
+++ src/git_review01/HelloWorld.java
@@ -5,5 +5,18 @@ public class HelloWorld  {
     public static void main(String[] args) {
 
         System.out.println(" This is Hello World Class");
+
+        System.out.println(" Hello Cydeo");
     }
 }
+/*
+If the color is white, that means it's already been saved in local repository.
+If the color is green, that means it is saved in their staging area, but has not been saved in local repository.
+If the color is red. that means
+the changes are made in the working directory, but they have not been saved in staging area yet.
+If it is in blue color. that means
+the chains that are made in the changes that are made in the working directory.
+and it the file was also saved in the staging area.
+The file was also saved in the local repository.
+But after saving those files in the staging area and local repository, you still need some change to the working directory. That's why, you have the blue color.
+ */
